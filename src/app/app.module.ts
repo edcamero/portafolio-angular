b8005fc61732e1f697c362fa9c7f7a1ce5cede19
Enyerson CAmero <blade_liger13@hotmail.com>
2019-03-13 08:16:21 -0500

    plantilla lista sin enlaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CertificadosComponent
   ],
   imports: [
     BrowserModule,
