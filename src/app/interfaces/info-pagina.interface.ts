80001fad28f43d881ca3092a583f160e16b1c255
edcamero <blade_liger13@hotmail.com>
2019-03-24 16:18:05 -0500

    Se agrega Acerca de mi al portafolio, llamando la informacion desde data-info.json se arregla error en la interface

--- src/app/interfaces/info-pagina.interface.ts
+++ src/app/interfaces/info-pagina.interface.ts
@@ -1,10 +1,10 @@
 
-export interface InfoPagina {
-  titulo: string;
-  sobremi: string;
-  email: string;
-  alias: string;
-  facebook: string;
-  twitter: string;
-  instagram: string;
+export interface InfoPagina{
+  titulo?: string;
+  sobremi?: string;
+  email?: string;
+  alias?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
 }
